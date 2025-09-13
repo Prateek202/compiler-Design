2a60c375e3de6b510dffecbc6f258fc0cb4fcd58
Prateek202 <115573650+Prateek202@users.noreply.github.com>
2025-09-13 10:55:37 +0530

    Update lab1.c

--- lab1.c
+++ lab1.c
@@ -1,52 +1,71 @@
-
 #include <stdio.h>
-#include <ctype.h>
 #include <string.h>
+#include <ctype.h>
 
-int isKeyword(char *str) {
-    char keywords[][10] = {"if", "else", "while", "for", "return"};
-    for (int i = 0; i < 5; i++) {
-        if (strcmp(str, keywords[i]) == 0)
-            return 1;
-    }
+int isKeyword(char *word) {
+    if (strcmp(word, "if") == 0 || strcmp(word, "else") == 0)
+        return 1;
     return 0;
 }
 
 int main() {
-    char ch, str[20];
-    int i = 0;
+    char input[100];
+    char token[20];
+    int i = 0, j = 0;
 
-    FILE *fp = fopen("input.txt", "r");
+    printf("Enter an IF statement:\n");
+    gets(input);  
 
-    if (!fp) {
-        printf("Cannot open file.\n");
-        return 1;
-    }
+    printf("\nLexical Analysis:\n");
+    while (input[i] != '\0') {
+        if (isspace(input[i])) {
+            i++;
+            continue;
+        }
 
-    while ((ch = fgetc(fp)) != EOF) {
-        if (isalnum(ch)) {
-            str[i++] = ch;
-        } else if ((ch == ' ' || ch == '\n' || ch == '\t') && i > 0) {
-            str[i] = '\0';
-            i = 0;
-
-            if (isKeyword(str))
-                printf("KEYWORD: %s\n", str);
-            else if (isdigit(str[0]))
-                printf("NUMBER: %s\n", str);
+        // identifiers/keywords
+        if (isalpha(input[i])) {
+            j = 0;
+            while (isalnum(input[i])) {
+                token[j++] = input[i++];
+            }
+            token[j] = '\0';
+            if (isKeyword(token))
+                printf("%s : Keyword\n", token);
             else
-                printf("IDENTIFIER: %s\n", str);
+                printf("%s : Identifier\n", token);
         }
 
-        if (ch == '<' || ch == '>' || ch == '=' || ch == '+' || ch == '-' || ch == '*') {
-            printf("OPERATOR: %c\n", ch);
+        // numbers
+        else if (isdigit(input[i])) {
+            j = 0;
+            while (isdigit(input[i])) {
+                token[j++] = input[i++];
+            }
+            token[j] = '\0';
+            printf("%s : Number\n", token);
         }
 
-        if (ch == '(' || ch == ')' || ch == '{' || ch == '}' || ch == ';') {
-            printf("SYMBOL: %c\n", ch);
+        // operators and symbols
+        else {
+            if (input[i] == '<' || input[i] == '>' || input[i] == '=' || input[i] == '!') {
+                if (input[i+1] == '=') {
+                    printf("%c%c : Relational Operator\n", input[i], input[i+1]);
+                    i += 2;
+                } else {
+                    printf("%c : Relational Operator\n", input[i]);
+                    i++;
+                }
+            }
+            else if (input[i] == '(' || input[i] == ')' || input[i] == '{' || input[i] == '}') {
+                printf("%c : Bracket\n", input[i]);
+                i++;
+            }
+            else {
+                printf("%c : Unknown Symbol\n", input[i]);
+                i++;
+            }
         }
     }
-
-    fclose(fp);
     return 0;
 }
